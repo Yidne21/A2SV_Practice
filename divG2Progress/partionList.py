f44fe6e1ecfc6997802cc982f0515111889beda9
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-06-22 22:52:18 +0300

    solution for partion list problem

---
+++ divG2Progress/partionList.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        left, right = ListNode(), ListNode()
+        leftTail, rightTail = left, right
+
+        while head:
+            if head.val < x:
+                leftTail.next = head
+                leftTail = leftTail.next
+            else:
+                rightTail.next = head
+                rightTail = rightTail.next
+            head = head.next
+        leftTail.next = right.next
+        rightTail.next = None
+        return left.next
