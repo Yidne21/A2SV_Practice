040d99c9b51d5bb1613234bc779a652a50455fa5
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-07-03 12:55:33 +0300

    solve remove dublicates from a sorted list II problem

---
+++ divG2Progress/removeDublicatesFromListII.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0, head)
+        left = dummy
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                left.next = head
+            else:
+                head = head.next
+                left = left.next
+        
+        return dummy.next
+
